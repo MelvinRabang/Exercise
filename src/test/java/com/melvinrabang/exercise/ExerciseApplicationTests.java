fbd9692d53c5c32f19a80186a754daa6eae9dc0f
mirabang_88@yahoo.com <mirabang_88@yahoo.com>
2021-07-14 23:18:46 +0800

    Initial commit exercise

---
+++ src/test/java/com/melvinrabang/exercise/ExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melvinrabang.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
